7366a583f1a2ab1119dd79896c26bce045ed21fb
John Gardner <john@ardea.io>
2025-01-10 08:34:34 -0500

    Initial

---
+++ quantum_examples/main.py
@@ -0,0 +1,10 @@
+import dimod
+from dwave.system import DWaveSampler, EmbeddingComposite, LeapHybridSampler
+from dwave.embedding import verify_embedding
+
+
+def main():
+    print('Hello, World!')
+
+if __name__ == '__main__':
+    main()
